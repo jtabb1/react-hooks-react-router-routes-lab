9ec98181604a32e5183a00c9838783886b870cc8
Jeremiah <jeremiah.r29q7.tabb@gmail.com>
2021-05-26 13:07:03 -0400

    Complete assignment fc

--- src/index.js
+++ src/index.js
@@ -9,3 +9,17 @@ ReactDOM.render(
   </Router>,
   document.getElementById("root")
 );
+
+/*
+
+touch src/data.js
+touch src/index.js
+mkdir src/components
+touch src/components/Actors.js
+touch src/components/App.js
+touch src/components/Directors.js
+touch src/components/Home.js
+touch src/components/Movies.js
+touch src/components/NavBar.js
+
+*/
